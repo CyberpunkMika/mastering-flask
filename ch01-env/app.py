f23516c482401145a392c14ab31ccf92e95c5575
Mark Hawkins <mark.hawkins@gmail.com>
2024-09-17 05:39:41 +0100

    Initial Commit

---
+++ ch01-env/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return "This is an online ... counseling system (OPCS)"
+
+
+@app.route("/home")
+def home():
+    return """
+        <html><head><title>Online Personal ... System</title>
+            </head><body>
+                <h1>Online ... Counseling System (OPCS)</h1>
+                <p>This is a template of a web-based counseling 
+                application where counselors can ... ... ...</p>
+                </body></html>
+    """
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
